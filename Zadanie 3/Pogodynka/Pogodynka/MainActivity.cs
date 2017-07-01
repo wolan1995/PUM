edb09d5267eb4fb913e343260b215d8f5c0e709d
wolan1995 <wolan1995@gmail.com>
2017-07-02 00:55:10 +0200

    Dodano Zadanie 3

---
+++ Zadanie 3/Pogodynka/Pogodynka/MainActivity.cs
@@ -0,0 +1,222 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Threading.Tasks;
+using System.Net;
+using System;
+using System.IO;
+using System.Json;
+using Android.Locations;
+using System.Collections.Generic;
+using Android.Util;
+using System.Linq;
+using System.Text;
+
+namespace Pogodynka
+{
+    [Activity(Label = "Pogodynka", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity, ILocationListener
+    {
+        public async void OnLocationChanged(Location location)
+        {
+            _currentLocation = location;
+            if (_currentLocation == null)
+            {
+                _locationText.Text = "Unable to determine your location. Try again in a short while.";
+            }
+            else
+            {
+                _locationText.Text = string.Format("{0:f6},{1:f6}", _currentLocation.Latitude, _currentLocation.Longitude);
+                string latitudeString = string.Format("{0:f6}", _currentLocation.Latitude);
+                latitude.Text = latitudeString.Replace(",", ".");
+                string longitudeString = string.Format("{0:f6}", _currentLocation.Longitude);
+                longitude.Text = longitudeString.Replace(",", ".");
+                Address address = await ReverseGeocodeCurrentLocation();
+                DisplayAddress(address);
+            }
+        }
+
+        public void OnProviderDisabled(string provider) { }
+
+        public void OnProviderEnabled(string provider) { }
+
+        public void OnStatusChanged(string provider, Availability status, Bundle extras) { }
+
+        static readonly string TAG = "X:" + typeof(MainActivity).Name;
+        TextView _addressText;
+        Location _currentLocation;
+        LocationManager _locationManager;
+        EditText latitude;
+        EditText longitude;
+
+        string _locationProvider;
+        TextView _locationText;
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            _locationManager.RequestLocationUpdates(_locationProvider, 0, 0, this);
+        }
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _locationManager.RemoveUpdates(this);
+        }
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            _addressText = FindViewById<TextView>(Resource.Id.address_text);
+            _locationText = FindViewById<TextView>(Resource.Id.location_text);
+            FindViewById<TextView>(Resource.Id.get_address_button).Click += AddressButton_OnClick;
+
+            InitializeLocationManager();
+
+            // Get the latitude/longitude EditBox and button resources:
+            latitude = FindViewById<EditText>(Resource.Id.latText);
+            longitude = FindViewById<EditText>(Resource.Id.longText);
+            Button button = FindViewById<Button>(Resource.Id.getWeatherButton);
+
+            // When the user clicks the button ...
+            button.Click += async (sender, e) => {
+
+                // Get the latitude and longitude entered by the user and create a query.
+                string url = "http://api.geonames.org/findNearByWeatherJSON?lat=" +
+                             latitude.Text +
+                             "&lng=" +
+                             longitude.Text +
+                             "&username=wolan1995";
+                // Fetch the weather information asynchronously, 
+                // parse the results, then update the screen:
+                JsonValue json = await FetchWeatherAsync(url);
+                ParseAndDisplay (json);
+            };
+        }
+
+        async void AddressButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            if (_currentLocation == null)
+            {
+                _addressText.Text = "Can't determine the current address. Try again in a few minutes.";
+                return;
+            }
+
+            Address address = await ReverseGeocodeCurrentLocation();
+            DisplayAddress(address);
+        }
+
+        async Task<Address> ReverseGeocodeCurrentLocation()
+        {
+            Geocoder geocoder = new Geocoder(this);
+            IList<Address> addressList =
+                await geocoder.GetFromLocationAsync(_currentLocation.Latitude, _currentLocation.Longitude, 10);
+
+            Address address = addressList.FirstOrDefault();
+            return address;
+        }
+
+        void DisplayAddress(Address address)
+        {
+            if (address != null)
+            {
+                StringBuilder deviceAddress = new StringBuilder();
+                for (int i = 0; i < address.MaxAddressLineIndex; i++)
+                {
+                    deviceAddress.AppendLine(address.GetAddressLine(i));
+                }
+                // Remove the last comma from the end of the address.
+                _addressText.Text = deviceAddress.ToString();
+            }
+            else
+            {
+                _addressText.Text = "Unable to determine the address. Try again in a few minutes.";
+            }
+        }
+
+        void InitializeLocationManager()
+        {
+            _locationManager = (LocationManager)GetSystemService(LocationService);
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Fine
+            };
+            IList<string> acceptableLocationProviders = _locationManager.GetProviders(criteriaForLocationService, true);
+
+            if (acceptableLocationProviders.Any())
+            {
+                _locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                _locationProvider = string.Empty;
+            }
+            Log.Debug(TAG, "Using " + _locationProvider + ".");
+        }
+
+        // Gets weather data from the passed URL.
+        private async Task<JsonValue> FetchWeatherAsync(string url)
+        {
+            // Create an HTTP web request using the URL:
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(new Uri(url));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            // Send the request to the server and wait for the response:
+            using (WebResponse response = await request.GetResponseAsync())
+            {
+                // Get a stream representation of the HTTP web response:
+                using (Stream stream = response.GetResponseStream())
+                {
+                    // Use this stream to build a JSON document object:
+                    JsonValue jsonDoc = await Task.Run(() => JsonObject.Load(stream));
+                    Console.Out.WriteLine("Response: {0}", jsonDoc.ToString());
+
+                    // Return the JSON document:
+                    return jsonDoc;
+                }
+            }
+        }
+
+        private void ParseAndDisplay(JsonValue json)
+        {
+            // Get the weather reporting fields from the layout resource:
+            TextView location = FindViewById<TextView>(Resource.Id.locationText);
+            TextView temperature = FindViewById<TextView>(Resource.Id.tempText);
+            TextView humidity = FindViewById<TextView>(Resource.Id.humidText);
+            TextView conditions = FindViewById<TextView>(Resource.Id.condText);
+
+            // Extract the array of name/value results for the field name "weatherObservation". 
+            JsonValue weatherResults = json["weatherObservation"];
+
+            // Extract the "stationName" (location string) and write it to the location TextBox:
+            location.Text = weatherResults["stationName"];
+
+            // The temperature is expressed in Celsius:
+            double temp = weatherResults["temperature"];
+            // Convert it to Fahrenheit:
+            temp = ((9.0 / 5.0) * temp) + 32;
+            // Write the temperature (one decimal place) to the temperature TextBox:
+            temperature.Text = String.Format("{0:F1}", temp) + "° F";
+
+            // Get the percent humidity and write it to the humidity TextBox:
+            double humidPercent = weatherResults["humidity"];
+            humidity.Text = humidPercent.ToString() + "%";
+
+            // Get the "clouds" and "weatherConditions" strings and 
+            // combine them. Ignore strings that are reported as "n/a":
+            string cloudy = weatherResults["clouds"];
+            if (cloudy.Equals("n/a"))
+                cloudy = "";
+            string cond = weatherResults["weatherCondition"];
+            if (cond.Equals("n/a"))
+                cond = "";
+
+            // Write the result to the conditions TextBox:
+            conditions.Text = cloudy + " " + cond;
+        }
+
+
+    }
+}
+
