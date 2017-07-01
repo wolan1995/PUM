5df9af63bd5cdc4daacbd84df79e9f8971d1a2a9
wolan1995 <wolan1995@gmail.com>
2017-07-02 00:40:28 +0200

    Dodano Zadanie 1

---
+++ Zadanie 1/Zadanie 2/MainActivity.cs
@@ -0,0 +1,80 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace Zadanie_2
+{
+    [Activity(Label = "Dzwon, SMSuj, MAILuj", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button actionButton;
+        private EditText addressText;
+        private EditText messageText;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView (Resource.Layout.Main);
+
+            actionButton = FindViewById<Button>(Resource.Id.actionButton);
+            addressText = FindViewById<EditText>(Resource.Id.addres);
+            messageText = FindViewById<EditText>(Resource.Id.message);
+
+
+
+            actionButton.Click += (object sender, EventArgs e) =>
+            {
+                int n;
+                bool isNumeric = int.TryParse(addressText.Text, out n);
+
+                if (isNumeric && String.IsNullOrWhiteSpace(messageText.Text))
+                {
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + addressText.Text));
+                    StartActivity(callIntent);
+                }
+
+                else if (isNumeric && !String.IsNullOrWhiteSpace(messageText.Text))
+                {
+                    var smsUri = Android.Net.Uri.Parse("smsto:" + addressText.Text);
+                    var smsIntent = new Intent(Intent.ActionSendto, smsUri);
+                    smsIntent.PutExtra("sms_body", messageText.Text);
+                    StartActivity(smsIntent);
+                }
+                else if (String.IsNullOrWhiteSpace(messageText.Text) && String.IsNullOrWhiteSpace(addressText.Text))
+                {
+                    Toast.MakeText(this, "Nie podales zadnych danych", ToastLength.Long).Show();
+                }
+                else
+                {
+                    var email = new Intent(Android.Content.Intent.ActionSend);
+                    email.SetType("message/rfc822");
+
+                    email.PutExtra(Android.Content.Intent.ExtraEmail, addressText.Text);
+                    email.PutExtra(Android.Content.Intent.ExtraText, messageText.Text);
+
+
+                    try
+                    {
+                        StartActivity(email);
+                    }
+                    catch (Android.Content.ActivityNotFoundException ex)
+                    {
+
+                            Toast.MakeText(this, "Brak aplikacji do wysylania e-maili!", ToastLength.Long).Show();
+
+                    }
+                }
+            };
+
+
+
+        }
+
+
+    }
+}
+
---
+++ Zadanie 1/Zadanie 2/obj/Debug/android/src/md55c2b3de823aee70a4271a803c5c9a885/MainActivity.java
@@ -0,0 +1,47 @@
+package md55c2b3de823aee70a4271a803c5c9a885;
+
+
+public class MainActivity
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Zadanie_2.MainActivity, Zadanie 2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Zadanie_2.MainActivity, Zadanie 2, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
